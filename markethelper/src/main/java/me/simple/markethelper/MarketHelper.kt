62f3230bddc50a157c5fda6c1b9439a4cd80d52d
陈鹏 <simple19930611@gmail.com>
2020-06-04 19:52:40 +0800

    add：README

--- markethelper/src/main/java/me/simple/markethelper/MarketHelper.kt
+++ markethelper/src/main/java/me/simple/markethelper/MarketHelper.kt
@@ -9,26 +9,30 @@ import java.util.*
 object MarketHelper {
 
     //第三方市场的包名
-    const val qihoo_360 = "com.qihoo.appstore"//360市场
+    const val QIHOO_360 = "com.qihoo.appstore"//360市场
     const val YING_YONG_HUI = "com.yingyonghui.market"//应用汇
+    const val YING_YONG_BAO = "com.tencent.android.qqdownloader"//应用宝
+    const val BAI_DU = "com.baidu.appsearch"//百度手机助手
+    const val WAN_DOU_JIA = "com.wandoujia.phoenix2"//豌豆荚
+    const val AN_ZHI = "cn.goapk.market"//安智市场
+    const val KU_AN = "com.coolapk.market"//酷安
 
     //key=系统，value=商店包名
     val marketMap = TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER)
 
     init {
-        marketMap["Google"] = "com.android.vending"//Google Pixel
-        marketMap["OnePlus"] = "com.oppo.market"//一加
-        marketMap["Xiaomi"] = "com.xiaomi.market"//小米，红米
-        marketMap["Meizu"] = "com.meizu.mstore"//魅族
-        marketMap["HUAWEI"] = "com.huawei.appmarket"//华为
-        marketMap["sangsume"] = ""//三星
-        marketMap["OPPO"] = "com.oppo.market"//oppo
-        marketMap["vivo"] = ""//vivo
-        marketMap[""] = ""//联想
+        marketMap["Google"] = "com.android.vending"//Google Pixel-已测试
+        marketMap["OnePlus"] = "com.oppo.market"//一加-已测试
+        marketMap["Xiaomi"] = "com.xiaomi.market"//小米，红米-已测试
+        marketMap["Meizu"] = "com.meizu.mstore"//魅族-已测试
+        marketMap["OPPO"] = "com.oppo.market"//oppo-已测试
+        marketMap["HUAWEI"] = "com.huawei.appmarket"//华为，荣耀-已测试
+        marketMap["SAMSUNG"] = "com.sec.android.app.samsungapps"//三星
+        marketMap["VIVO"] = "com.bbk.appstore"//vivo
+        marketMap["lenovo"] = "com.lenovo.leos.appstore"//联想
         marketMap[""] = ""//红魔
-        marketMap[""] = ""//荣耀
         marketMap[""] = ""//黑鲨
-        marketMap["realme"] = ""//realme
+        marketMap[""] = ""//realme
         marketMap[""] = ""//海信
     }
 
